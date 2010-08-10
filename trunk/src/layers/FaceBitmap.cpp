cedb9b03098779e757bbb6000845d2460f5d6e24
jaromir.vitek <jaromir.vitek@705d5ebd-0256-0410-8e7e-655382512755>
2010-08-10 10:34:46 +0000

    - Added offset to ShadowInset - FaceBitmap set pattern extend based on current repeat setting. When repeat is equal set CAIRO_EXTEND_PAD otherwise CAIRO_EXTEND_REPEAT

--- trunk/src/layers/FaceBitmap.cpp
+++ trunk/src/layers/FaceBitmap.cpp
@@ -19,43 +19,6 @@ GlyphLayerFaceBitmap::~GlyphLayerFaceBitmap() {
 	cairo_pattern_destroy(_pattern);
 }
 
-/*
-void setSubImageAlpha(
-	cairo_surface_t* surface,
-	unsigned int x,
-	unsigned int y,
-	unsigned int w,
-	unsigned int h,
-	unsigned char alpha
-) {
-	unsigned char* data   = cairo_image_surface_get_data(surface);
-	cairo_format_t format = cairo_image_surface_get_format(surface);
-	int            stride = cairo_image_surface_get_stride(surface);
-
-	unsigned int channels = 0;
-	unsigned int offset = 0;
-
-	if(format == CAIRO_FORMAT_ARGB32) {
-		channels = 4;
-		offset = 3;
-	}
-
-	else if(format == CAIRO_FORMAT_A8) { 
-		channels = 1;
-		offset = 0;
-	}
-
-	else return;
-
-	for(unsigned int iY = y; iY < y + h; iY++) {
-		for(unsigned int iX = x; iX < x + w; iX++) {
-			unsigned char* dataPtr = &data[iY * stride + iX * channels];
-			dataPtr[offset] = alpha;
-		}
-	}
-}
-*/
-
 bool GlyphLayerFaceBitmap::render(
 	cairo_t*       c,
 	cairo_glyph_t* glyph,
@@ -67,6 +30,10 @@ bool GlyphLayerFaceBitmap::render(
 	double bw = _bitmap->getSurfaceWidth();
 	double bh = _bitmap->getSurfaceHeight();
 
+	if(_repeatX > 1 || _repeatY > 1) cairo_pattern_set_extend(_pattern, CAIRO_EXTEND_REPEAT);
+	
+	else cairo_pattern_set_extend(_pattern, CAIRO_EXTEND_PAD);
+	
 	cairo_matrix_t scale_matrix;
 
 	cairo_matrix_init_scale(&scale_matrix, bw / width * _repeatX, bh / height * _repeatY);
@@ -102,9 +69,6 @@ void GlyphLayerFaceBitmap::setBitmap(const std::string& path) {
 	_pattern = cairo_pattern_create_for_surface(_bitmap->getSurface());
 	
 	if(cairo_pattern_status(_pattern)) return;
-
-	cairo_pattern_set_extend(_pattern, CAIRO_EXTEND_REPEAT);
-	cairo_pattern_set_filter(_pattern, CAIRO_FILTER_BEST);
 }
 
 } 
